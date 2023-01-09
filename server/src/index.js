74358ff669bbfd164d7418cdde3c3f0b14fd4dfa
Anna-Maria Tipotsch c102371 <anna-maria.tipotsch@uibk.ac.at>
2023-01-09 15:59:18 +0100

    some new libs

--- server/src/index.js
+++ server/src/index.js
@@ -4,7 +4,11 @@ const cors = require("cors");
 const db = require("../config/db")
 const logger = require('../tools/logging');
 const { response } = require('express');
+const cookieParser = require("cookie-parser");
 const app = express()
+var genuuid = require('uuid');// '/v4' for version 4
+var session
+const sessions = require('express-session');
 const standartSha = 'cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e'
 /*if client has authentication issues:
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'pw';
@@ -12,19 +16,31 @@ flush privileges;
 
 in sql
 */
+
+//App.uses
 const port = 5000
 app.use(express.json())
+app.use(sessions(
+  { 
+    secret: 'gffjgfjhgfjgfjgdsfsflkizuv',
+    saveUninitialized: false,
+    cookie: { maxAge: 86400000 },  //One day: 86 400 000
+    resave: false 
+  }));
 app.use(cors({
     origin: "http://localhost:3000",
 }))
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
+//listen to port
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+//list of API point
  app.post('/api/registration', async (req, res)=>{
     const username = req.body.user
     const pw = req.body.password
@@ -73,18 +89,20 @@ app.post("/api/login", async(req, res) => {
   console.log("login")
   let email = req.body.user
   let pw = req.body.pw
-  console.log(email)
   if(email && pw){
     db.query('SELECT * FROM user WHERE email = ? AND pw = ?', [email, pw], (err, result)=> {
       console.log(result)
+      console.log(Object.hasOwn(result, "username"))
       if(err) {
         console.log(err)
-      if(result.length > 0){
-        req.session.loggedin = true;
-				req.session.username = username;
+      if(Object.hasOwn(result, "username")){   //Find posiblity to check if input was correct TODO
+        console.log(req.session + " NEW")
+				session=req.session;
+        session.userid=req.body.user;
+        console.log(req.session);
 				// Redirect to home page
 				//res.redirect('/home');
-        res.send("logged in")
+        res.send("<h1>logged in</h1>")
       }
       else {
         res.send("incorrect email or password")
@@ -97,7 +115,7 @@ app.post("/api/login", async(req, res) => {
 
 app.get('/home', function(request, response) {
 	// If the user is loggedin
-	if (request.session.loggedin) {
+	if (request.session.username = username) {
 		// Output username
 		response.send('Welcome back, ' + request.session.username + '!');
 	} else {
@@ -105,6 +123,16 @@ app.get('/home', function(request, response) {
 		response.send('Please login to view this page!');
 	}
 	response.end();
+});
+
+// test for session management
+app.get('/secret',(req,res)=>{
+  session=req.session;
+  console.log(session.userid + "    SES")
+  if(session.userid){
+      res.send("<div>LOGIN</div>");
+  }else
+  res.send("<div>FALSCHER LOGIN</div>")
 });
 
 console.log("API Started")
